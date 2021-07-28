9b3ca573cea00d3d0bca01567941eae689ada555
Diego Almeida <diego.almeida@outlook.com>
2021-07-28 00:32:30 -0300

    Servidor de envio de e-mail alterado

--- utils/email/sendEmail.js
+++ utils/email/sendEmail.js
@@ -6,8 +6,8 @@ const sendEmail = async (email, subject, text) => {
 
             const transporter = nodemailer.createTransport({
                 host: process.env.EMAIL_HOST,
-                port: 465,
-                secure: true,
+                port: 587,
+                secure: false,
                 auth: {
                     user: process.env.EMAIL_USERNAME,
                     pass: process.env.EMAIL_PASSWORD
