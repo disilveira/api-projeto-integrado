98456a3ecf1f66ffe3933d48c4323a7d16e12780
Diego Almeida <diego.almeida@outlook.com>
2021-07-29 15:31:39 -0300

    Alterada a forma de exibição da data nos gráficos

--- controllers/trips.controller.js
+++ controllers/trips.controller.js
@@ -2,7 +2,7 @@ const mysql = require('../config/mysql');
 
 exports.getTripsData = async (req, res, next) => {
     try {
-        const result = await mysql.execute("SELECT DATE_FORMAT(tripDate, '%Y%m%d') as date, SUM(tripKm) as tripKm, ROUND((SUM(tripTime)/60),2) as tripTime FROM trip_data GROUP BY date");
+        const result = await mysql.execute("SELECT DATE_FORMAT(tripDate, '%d/%m/%y') as date, SUM(tripKm) as tripKm, ROUND((SUM(tripTime)/60),2) as tripTime FROM trip_data GROUP BY date");
         return res.status(200).send(result);
     } catch (error) {
         return res.status(500).send({ error: error.message });
